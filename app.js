9ac80e37d40fbd379a477c028d205d6326897465
krishna30121 <57456339+krishna30121@users.noreply.github.com>
2022-09-02 15:23:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const url='mongodb://localhost/AlienDBex'
+const app=express()
+mongoose.connect(url,{useNewUrlParser:true})
+const con=mongoose.connection
+con.on('open',()=>
+{
+    console.log('connected...')
+})
+app.use(express.json())
+const alienRouter=require('../crudjs/routes/aliens')
+app.use('/aliens',alienRouter)
+app.listen(9000,()=>
+{
+    console.log('server started')
+})
